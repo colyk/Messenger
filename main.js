9236cf23f0433e7e0eea95a5fbbb580c5eaa6881
colyk <colyk@yandex.ru>
2018-04-29 23:19:51 +0200

    Start page

--- main.js
+++ main.js
@@ -1,31 +1,12 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server)
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
 
-io.on('connection', function (client) {
-  client.on('register', handleRegister)
 
-  client.on('join', handleJoin)
+app.use(express.static(__dirname + '/templates'));  
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
 
-  client.on('leave', handleLeave)
-
-  client.on('message', handleMessage)
-
-  client.on('chatrooms', handleGetChatrooms)
-
-  client.on('availableUsers', handleGetAvailableUsers)
-
-  client.on('disconnect', function () {
-    console.log('client disconnect...', client.id)
-    handleDisconnect()
-  })
-
-  client.on('error', function (err) {
-    console.log('received error from client:', client.id)
-    console.log(err)
-  })
-})
-
-server.listen(3000, function (err) {
-  if (err) throw err
-  console.log('listening on port 3000')
-})
+server.listen(5000);
