d679ffb7780cf6a2f8c84f6b2ba759c43835c9f4
colyk <colyk@yandex.ru>
2018-04-30 00:30:15 +0200

    Added redis

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
+var redis = require('redis');
+var client = redis.createClient();
 
 
 app.use(express.static(__dirname + '/templates'));  
