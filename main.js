685b42818774e7db7c90a56534acce4a7a27b2cf
colyk <colyk@yandex.ru>
2018-04-30 02:00:25 +0200

    simple chat example

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
 var client = redis.createClient();
 
-app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/templates/'));
 
-app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/templates/index.html');
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
-  });
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
 });
 
 server.listen(5000);
