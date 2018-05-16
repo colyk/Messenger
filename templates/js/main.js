69cb9c3388b5e1dfcacbb6df195716370ad38424
oleksandrkobzar <kobzar.sasha1@gmail.com>
2018-05-16 19:19:02 +0200

    edit user photo

--- templates/js/main.js
+++ templates/js/main.js
@@ -54,9 +54,9 @@ $(function() {
     }
     if (localStorage.nickname) {
         let nick = localStorage.nickname.toUpperCase();
-        $(".nick_photo").text(nick.charAt(0) + nick.charAt(1));
+        $(".user_photo").text(nick.charAt(0) + nick.charAt(1));
     } else {
-        $(".nick_photo").text("UN");
+        $(".user_photo").text("UN");
     }
 });
 //переворот стрілки
