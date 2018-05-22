6d00dd4393d30b9898cd230be5fa4888cdda4186
oleksandrkobzar <kobzar.sasha1@gmail.com>
2018-05-22 18:38:57 +0200

    new headerLeftBar and fluid height screen

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,68 +1,112 @@
 $(function() {
+    ///height screen
+    $("html").css({'height':$(window).height()});
+    $(".left_list").css({'height':$(window).height()});
+    $(".center_list").css({'height':$(window).height()});
+    $(".scroll").css({'height':$(".center_list").height()-120});
+    $(window).resize(function() {
+        $("html").css({'height':$(window).height()});
+        $(".left_list").css({'height':$(window).height()});
+        $(".center_list").css({'height':$(window).height()});
+        $(".scroll").css({'height':$(".center_list").height()-$(".header_panel").height()-$("#send_block").height()-30});
+        if($(window).height()>=768){
+        $('#active_but_bar2').addClass('active_but_bar');
+        }
+        else{
+            $('#active_but_bar2').addClass('active_but_bar');
+        }
+    })
+    if($(window).height()>=768)
+        $('#active_but_bar2').addClass('active_but_bar');
+    else
+        $('#active_but_bar2').addClass('active_but_bar');
     //scroll messages
     $("div.scroll.messages").scrollTop(300);
 
-    var eleml = document.getElementsByClassName('left_list')[0];
-    var elemc = document.getElementsByClassName('center_list')[0];
-    var flagl = 1;
-    document.getElementsByClassName('button_bar')[0].onclick = function() {
-        if (flagl == 0) {
-            eleml.style.display = 'block';
-            eleml.style.background = '#fff';
-            if (eleml.classList.contains("col-xl-0")) {
-                eleml.classList.remove("col-xl-0");
-                eleml.classList.add("col-xl-3");
-                elemc.classList.remove("col-xl-12");
-                elemc.classList.add("col-xl-9");
-            }
-            if (eleml.classList.contains("col-lg-0")) {
-                eleml.classList.remove("col-lg-0");
-                eleml.classList.add("col-lg-4");
-                elemc.classList.remove("col-lg-12");
-                elemc.classList.add("col-lg-8");
-            }
-            if (eleml.classList.contains("col-md-0")) {
-                eleml.classList.remove("col-md-0");
-                eleml.classList.add("col-md-4");
-                elemc.classList.remove("col-md-12");
-                elemc.classList.add("col-md-8");
-            }
+    var 
+        $leftList = $('.left_list'),
+        $centerList = $('.center_list'),
+        $activeBut = $('.active_but_bar'),
+        flagScreen = 1;
 
-            flagl = 1;
+    $activeBut.on('click', function() {
+        if (flagScreen == 0) {
+            $leftList.css({
+                'display':'block',
+                'background':'#fff'
+            });
+            if($leftList.hasClass('col-xl-0')){
+                $leftList
+                    .removeClass('col-xl-0')
+                    .addClass('col-xl-3');
+                $centerList
+                    .removeClass('col-xl-12')
+                    .addClass('col-xl-9');
+            }
+            if($leftList.hasClass('col-lg-0')){
+                $leftList
+                    .removeClass('col-lg-0')
+                    .addClass('col-lg-4');
+                $centerList
+                    .removeClass('col-lg-12')
+                    .addClass('col-lg-8');
+            }
+            if($leftList.hasClass('col-md-0')){
+                $leftList
+                    .removeClass('col-md-0')
+                    .addClass('col-md-4');
+                $centerList
+                    .removeClass('col-md-12')
+                    .addClass('col-md-8');
+            }
+            $('#active_but_bar2').removeClass('active_but_bar');
+            $('#active_but_bar1').addClass('active_but_bar');
+            flagScreen = 1;
         } else {
-            eleml.style.display = 'none';
-            if (eleml.classList.contains("col-xl-3")) {
-                eleml.classList.remove("col-xl-3");
-                eleml.classList.add("col-xl-0");
-                elemc.classList.remove("col-xl-9");
-                elemc.classList.add("col-xl-12");
+            $leftList.css({'display':'none'});
+            if($leftList.hasClass('col-xl-3')){
+                $leftList
+                    .removeClass('col-xl-3')
+                    .addClass('col-xl-0');
+                $centerList
+                    .removeClass('col-xl-9')
+                    .addClass('col-xl-12');
             }
-            if (eleml.classList.contains("col-lg-4")) {
-                eleml.classList.remove("col-lg-4");
-                eleml.classList.add("col-lg-0");
-                elemc.classList.remove("col-lg-8");
-                elemc.classList.add("col-lg-12");
+            if($leftList.hasClass('col-lg-4')){
+                $leftList
+                    .removeClass('col-lg-4')
+                    .addClass('col-lg-0');
+                $centerList
+                    .removeClass('col-lg-8')
+                    .addClass('col-lg-12');
             }
-            if (eleml.classList.contains("col-md-4")) {
-                eleml.classList.remove("col-md-4");
-                eleml.classList.add("col-md-0");
-                elemc.classList.remove("col-md-8");
-                elemc.classList.add("col-md-12");
+            if($leftList.hasClass('col-md-4')){
+                $leftList
+                    .removeClass('col-md-4')
+                    .addClass('col-md-0');
+                $centerList
+                    .removeClass('col-md-8')
+                    .addClass('col-md-12');
             }
-            flagl = 0;
+            $('#active_but_bar2').addClass('active_but_bar');
+            $('#active_but_bar1').removeClass('active_but_bar');
+            flagScreen = 0;
         }
-    }
+    });
     if (localStorage.nickname) {
         let nick = localStorage.nickname.toUpperCase();
         $(".my_photo").text(nick.charAt(0) + nick.charAt(1));
     } else {
         $(".my_photo").text("UN");
     }
+
+    //переворот стрілки
+    $activeBut.on('click', function() {
+        $('#active_but_bar2').toggleClass('rotate');
+        $('#active_but_bar1').toggleClass('rotate');
+    });
 });
-//переворот стрілки
-$(".button_bar").click(function() {
-    this.classList.toggle('rotate');
-});
+
 //завантаження файла(клік по полю)
 function clickFindFile() {
     document.getElementById('uploadfile').click();
